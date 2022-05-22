48eb8f726a996f23c72b8609bd98ffbc1d7e2d34
IvanMonsalve04 <ivanmonsalv8@gmail.com>
2022-05-22 16:03:05 -0500

    hghyyj

--- src/controll/Controller.java
+++ src/controll/Controller.java
@@ -58,68 +58,6 @@ public class Controller implements CustomEvent {
 	}
 
 	//Manejo de Archivos
-	public String verificarExistencia() {
-		objetoArchivos = new File("src\\resources\\RegistroClientes.txt"); //Puede ir en el
-		// constructor, pero si se utiliza esa ruta para todo el programa.
-		//objetoArchivos = new File("src\\resources\\rimero.txt");
-		if (objetoArchivos.exists()) { // Si el archivo existe...
-			nombre = objetoArchivos.getName(); // obtiene el nombre del archivo
-			ruta = objetoArchivos.getPath(); // obtiene la ruta
-			rutaTotal = objetoArchivos.getAbsolutePath(); // obtiene la ruta total desde el disco raíz
-
-			leer = objetoArchivos.canRead(); // Se puede leer?
-			escribir = objetoArchivos.canWrite(); // Se puede escribir?
-			tamanio = (objetoArchivos.length() / 1024); // Longitud del archivo en bytes, pasado a kilobytes
-			mensaje = nombre + "\n" + ruta + "\n" + rutaTotal + "\n" + tamanio; // Se concatenan las variables
-		} else {
-			// mensaje = "El archivo no existe.";
-			objetoArchivos = new File("src\\resources\\RegistroPersonas.txt");
-			// Como se está invocando algo fuera del entorno se genera una excepción, por lo
-			// tanto:
-			try {
-				escribirArchivo = new BufferedWriter(new FileWriter(objetoArchivos)); // Se crea el archivo
-				mensaje = "Se ha creado el archivo";
-			} catch (IOException e) {
-				e.printStackTrace();
-				mensaje = "El archivo no se puede crear.";
-			}
-		}
-		return objetoIOManagerDos.imprimirMensaje(mensaje); // Retorna hacia la clase IOManager, en el método imprimir
-															// mensaje con el parametro mensaje
-	}
-
-	public String escribirArchivo() {
-		objetoArchivos = new File("src\\resources\\RegistroPersonas.txt");
-		int suma = 23 + 27;
-		// Los archivos siempre se manda información de tipo String
-		// Como se está invocando algo fuera del entorno se genera una excepción, por lo
-		// tanto:
-		try {
-			escribirArchivo = new BufferedWriter(new FileWriter(objetoArchivos)); // Se crea el archivo
-			escribirArchivo.write("Hola mundo, soy Tadeo.");
-			escribirArchivo.newLine();
-			escribirArchivo.write("El resultado de la suma es: " + suma);
-			escribirArchivo.close();
-			mensaje = "Tu archivo se ha creado y ya tiene datos";
-		} catch (IOException e) {
-			e.printStackTrace();
-			mensaje = "El archivo no se puede crear.";
-		}
-		return objetoIOManagerDos.imprimirMensaje(mensaje);
-	}
-
-	public String leerArchivo() {
-		objetoArchivos = new File("src\\resources\\RegistroPersonas.txt");
-		try {
-			leerArchivo = new BufferedReader(new FileReader(objetoArchivos));
-			mensaje = leerArchivo.readLine();
-			leerArchivo.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			mensaje = "El archivo no se puede crear.";
-		}
-		return objetoIOManagerDos.imprimirMensaje(mensaje);
-	}
 
 	//Gets and Sets
 	public CustomEventRespons getEventoRespuesta() {
@@ -134,10 +72,6 @@ public class Controller implements CustomEvent {
 	public void init() {
 		IOManager objetoIOManager = new IOManager();
 		objetoIOManager.setVisible(true);
-		verificarExistencia();
-		escribirArchivo();
-		leerArchivo();
 	}
-
 	
 }
